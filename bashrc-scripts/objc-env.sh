08c34c27ab79bbab198ed4c536b78ce4a21e3fec
caitlyn <caitlynannefinn@gmail.com>
2017-05-15 23:20:24 -0400

    Added environment script for Objective-C.

---
+++ bashrc-scripts/objc-env.sh
@@ -0,0 +1,13 @@
+#!/usr/bin/env bash
+
+#------------------------------------------------------------------------------
+# objc.sh
+#   This script sets up environment vars and settings for Objective-C
+#   development.
+#------------------------------------------------------------------------------
+
+function objc-env {
+    # GNUSTEP Environment vars
+    . /usr/share/GNUstep/Makefiles/GNUstep.sh
+}
+
